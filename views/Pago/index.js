9982276508ff8fa1dd2d9cd6068a9a4a6974eab6
Reinaldo <landaetareinaldo143@gmail.com>
2024-04-18 20:44:38 -0400

    first commit

--- views/Pago/index.js
+++ views/Pago/index.js
@@ -50,26 +50,48 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 function mostrarCita(cita) {
-    const citasContainer = document.getElementById('citas-container');
+  const citasContainer = document.getElementById('citas-container');
 
-    cita.services.forEach(servicio => {
-        const citaElement = document.createElement('div'); // Mueve esta línea dentro del bucle
-        citaElement.classList.add('bg-stone-900', 'p-4', 'rounded-lg', 'w-full', 'mb-4');
+  cita.services.forEach(servicio => {
+      const citaElement = document.createElement('div');
+      citaElement.classList.add('bg-stone-800', 'p-4', 'rounded-lg', 'w-full', 'md:w-full', 'lg:w-full', 'mb-4'); // Modificado: añadido padding p-4
 
-        const citaContent = `
-            <div class="text-sm w-full">
-                <img src="/images/perfil.svg" class="w-10 flex gap-4">
-                <p class="text-sm"><strong>Cliente:</strong> ${cita.user.name}</p>
-                <p class="text-sm"><strong>Email:</strong> ${cita.user.email}</p>
-                <p class="text-sm"><strong>Fecha:</strong> ${cita.date}</p>
-                <p class="text-sm"><strong>Hora:</strong> ${cita.hour}</p>
-                <p class="text-sm"><strong>Servicio:</strong> ${servicio.service}</p>
-                <p class="text-sm"><strong>Duración:</strong> ${servicio.duration}</p>
-                <p class="text-sm"><strong>Precio:</strong> ${servicio.price}</p>
-            </div>
-        `;
+      const citaContent = `
+          <div class="text-sm">
+              <img src="/images/perfil.svg" class="w-10 flex">
+              <p class="text-sm"><strong>Cliente:</strong> ${cita.user.name}</p>
+              <p class="text-sm"><strong>Email:</strong> ${cita.user.email}</p>
+              <p class="text-sm"><strong>Fecha:</strong> ${cita.date}</p>
+              <p class="text-sm"><strong>Hora:</strong> ${cita.hour}</p>
+              <p class="text-sm"><strong>Servicio:</strong> ${servicio.service}</p>
+              <p class="text-sm"><strong>Duración:</strong> ${servicio.duration}</p>
+              <p class="text-sm"><strong>Precio:</strong> ${servicio.price}</p>
+          </div>
+          <button class="p-2 delete-cita-btn hover:bg-red-500 bg-red-600 text-white px-3 py-1 rounded mt-2">Eliminar</button> <!-- Modificado: añadido margen superior mt-2 -->
+      `;
 
-        citaElement.innerHTML = citaContent;
-        citasContainer.appendChild(citaElement); 
-    });
+      citaElement.innerHTML = citaContent;
+
+      // Agregar evento de click al botón de eliminar cita
+      const deleteCitaBtn = citaElement.querySelector('.delete-cita-btn');
+      deleteCitaBtn.addEventListener('click', async function() {
+          try {
+              // Eliminar la cita del DOM
+              citaElement.remove();
+              
+              // Eliminar la cita de la base de datos
+              const response = await axios.delete(`/api/citas/${cita.id}`, { withCredentials: true });
+              if (response.status === 200) {
+                  console.log('La cita ha sido eliminada exitosamente de la base de datos.');
+              } else {
+                  console.error('Error al eliminar la cita de la base de datos:', response.statusText);
+                  throw new Error('Error al eliminar la cita de la base de datos');
+              }
+          } catch (error) {
+              console.error('Error en la solicitud:', error);
+          }
+      });
+
+      citasContainer.appendChild(citaElement);
+  });
 }
